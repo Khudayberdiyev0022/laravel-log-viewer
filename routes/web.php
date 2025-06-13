7b3b43a00f1d0379656b80198534cdefa74c4259
Xamza A. Xudayberdiyev <xamza_xudayberdiyev@uzinfocom.local>
2025-06-13 16:37:21 +0500

    updated

--- routes/web.php
+++ routes/web.php
@@ -11,5 +11,11 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
+Route::get('/log', function () {
+  \Illuminate\Support\Facades\Log::info('test log');
+  \Illuminate\Support\Facades\Log::warning('Warning test log');
+  \Illuminate\Support\Facades\Log::channel('daily')->error('Warning test log');
+});
+
 Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
 Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
